dc250a790c234e86ad28686f6a621f7e37f76395
Strongest Number 9 <16169054+StrongestNumber9@users.noreply.github.com>
2024-04-11 17:14:46 +0300

    Adds docker entrypoint that will check/warn for missing configurations (#31)
    * Adds docker entrypoint that will check/warn for missing configurations
    
    * Adds clarification about files required in container

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ ! -f /config/config.properties ]; then
+    echo "ERROR: Missing '/config/config.properties', refusing to continue";
+    exit 1;
+fi;
+
+if [ -f /config/log4j2.xml ]; then
+    LOG4J_FILE="/config/log4j2.xml";
+else
+    echo "INFO: Missing '/config/log4j2.xml', using default logger config";
+    LOG4J_FILE="/opt/teragrep/lsh_01/etc/log4j2.xml";
+fi;
+
+exec /usr/bin/java -Dproperties.file=/config/config.properties -Dlog4j2.configurationFile=file:"${LOG4J_FILE}" -jar /opt/teragrep/lsh_01/share/lsh_01.jar
